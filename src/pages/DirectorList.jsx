0b2f6ad47285005ed82b911e68ca19de532286df
Nevin <n.boyd1@yahoo.com>
2025-10-17 15:25:45 -0700

    Added nav links, loading message, and outlet context

--- src/pages/DirectorList.jsx
+++ src/pages/DirectorList.jsx
@@ -1,17 +1,24 @@
+import { Link, useOutletContext } from "react-router-dom"
 
 const DirectorList = () => {
     // Replace me
-    const directors = []
-
-    const displayDirectors = directors.map(d => (
-        <li><a>{d.name}</a></li>
-    ))
+    const { directors } = useOutletContext()
+    if (!directors || directors.length === 0) {
+        return <p>Loading directors...</p>
+    }
 
     return (
         <ul>
-            {displayDirectors}
+            {directors.map((d) => (
+                <li key={d.id}>
+                    <Link to={`/directors/${d.id}`}>{d.name}</Link>
+                </li>
+            ))}
+            <li>
+                <Link to="/directors/new">Add New Director</Link>
+            </li>
         </ul>
-    );
+    )
 }
 
 export default DirectorList;
