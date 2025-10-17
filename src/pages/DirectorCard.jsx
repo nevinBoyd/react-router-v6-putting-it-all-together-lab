23262c7c3ff65fb34161bf75cef9b45fb94a6de0
Nevin <n.boyd1@yahoo.com>
2025-10-17 15:29:50 -0700

    Added comments and implemented params, outlet context, and nested links

--- src/pages/DirectorCard.jsx
+++ src/pages/DirectorCard.jsx
@@ -1,26 +1,34 @@
+import { Link, Outlet, useOutletContext, useParams } from "react-router-dom"
 
 function DirectorCard() {
-    // Replace me
-    const director = null
-
+    const { id } = useParams()
+    const { directors } = useOutletContext()
+    // convert id from string - number to match data
+    const director = directors.find((d) => d.id === Number(id))
     if (!director) {
         return <h2>Director not found.</h2>
     }
 
     return (
         <div>
-        <h2>{director.name}</h2>
-        <p>{director.bio}</p>
-        <h3>Movies:</h3>
-        <ul>
-            {director.movies.map((movie) => (
-            <li key={movie.id}>
-                <a>{movie.title}</a>
-            </li>
-            ))}
-        </ul>
-        <Link to={`movies/new`}>Add New Movie</Link>
-        {/* Movie compoenents should render here depending on route */}
+            <h2>{director.name}</h2>
+            <p>{director.bio}</p>
+
+            <h3>Movies:</h3>
+            <ul>
+                {director.movies.map((movie) => (
+                    <li key={movie.id}>
+                        {/* Link for navigation to nested route */}
+                        <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+                    </li>
+                ))}
+            </ul>
+
+            {/* route for adding new movies */}
+            <Link to={`movies/new`}>Add New Movie</Link>
+
+            {/* nested routes rendered */}
+            <Outlet context={{ director }} />
         </div>
     )
 }
