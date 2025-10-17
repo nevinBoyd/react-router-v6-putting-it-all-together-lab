5dda11c264b58eee4b562c2dbfcd2bf5a39d81e6
Nevin <n.boyd1@yahoo.com>
2025-10-17 15:44:38 -0700

    Added route structure, nested director and movie routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import DirectorContainer from "./pages/DirectorContainer"
+import DirectorList from "./pages/DirectorList"
+import DirectorForm from "./pages/DirectorForm"
+import DirectorCard from "./pages/DirectorCard"
+import MovieForm from "./pages/MovieForm"
+import MovieCard from "./pages/MovieCard"
+
 const App = () => {
     return (
-        <>
-        {/* define all routes here */}
-        </>
+        <Routes>
+            {/* root route */}
+            <Route path="/" element={<Home />} />
+
+            {/* about route */}
+            <Route path="/about" element={<About />} />
+
+            {/* directors parent route */}
+            <Route path="/directors" element={<DirectorContainer />}>
+
+                {/* nested routes */}
+                <Route path index element={<DirectorList />} />
+                <Route path="new" element={<DirectorForm />} />
+                <Route path=":id" element={<DirectorCard />}>
+                    <Route path="movies/new" element={<MovieForm />} />
+                    <Route path="movies/:movieId" element={<MovieCard />} />
+                </Route>
+            </Route>
+        </Routes >
     )
 }
 
