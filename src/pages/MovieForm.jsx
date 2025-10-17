a7a7283d020f798a45bd656567fe1912bd48763b
Nevin <n.boyd1@yahoo.com>
2025-10-17 16:48:50 -0700

    Added navigation and context logic to MovieForm

--- src/pages/MovieForm.jsx
+++ src/pages/MovieForm.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid'
+import { useParams, useNavigate, useOutletContext } from "react-router-dom"
 
 function MovieForm() {
   const [title, setTitle] = useState("")
   const [time, setTime] = useState("")
   const [genres, setGenres] = useState("")
+  const { id } = useParams()
+  const { directors, setDirectors } = useOutletContext()
+  const navigate = useNavigate()
 
-  // Replace me
-  const director = null
-  
-  if (!director) { return <h2>Director not found.</h2>}
+  const director = directors.find((d) => d.id === Number(id))
+  if (!director) return <h2>Director not found.</h2>
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
     const newMovie = {
       id: uuidv4(),
       title,
@@ -24,18 +27,20 @@ function MovieForm() {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({movies: [...director.movies, newMovie]})
+      body: JSON.stringify({ movies: [...director.movies, newMovie] })
     })
-    .then(r => {
-      if (!r.ok) { throw new Error("failed to add movie") }
-      return r.json()
-    })
-    .then(data => {
-      console.log(data)
-      // handle context/state changes
-      // navigate to newly created movie page
-    })
-    .catch(console.log)
+      .then(r => {
+        if (!r.ok) { throw new Error("failed to add movie") }
+        return r.json()
+      })
+      .then(data => {
+        const updatedList = directors.map((d) =>
+          d.id === updatedDirector.id ? updatedDirector : d
+        )
+        setDirectors(updatedList)        // handle context/state changes
+        navigate(`movies/${newMovie}`)   // navigate to newly created movie page     
+      })
+      .catch(console.log)
   }
 
   return (
