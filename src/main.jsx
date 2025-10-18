d55f77757ee50c1d98f74ef90bb8500c3071ada8
Nevin <n.boyd1@yahoo.com>
2025-10-17 22:28:02 -0700

    Remove duplicate BrowserRouter wrapper in main.jsx to fix nested router error

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import "./index.css"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
-// wrap app BrowserRouter enabe routing
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-)
+root.render(<App />)
